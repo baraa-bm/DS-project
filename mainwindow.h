bf4f853c06281d0512a29e716c294cb16ad73da0
MoHussein95 <mohamedhussein@aucegypt.edu>
2026-05-14 02:29:56 +0300

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -14,7 +14,7 @@ class MainWindow : public QDialog
 
 public:
     manager *Task_Manager = nullptr;
-    Time *currentTime = new Time(5,0);
+    Time *currentTime = new Time(5, 0);
     int simulatedMinutes = 0;
 
     void displayTime(Time time);
