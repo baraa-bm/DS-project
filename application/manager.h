5b1d4d24f564e1b907430fef2029f2af168dff70
1kareem0 <karoomzaqout@gmail.com>
2026-05-13 21:14:04 +0300

    idk

--- application/manager.h
+++ application/manager.h
@@ -1,10 +1,10 @@
 #ifndef ADS_PROJECT_MANAGER_H
 #define ADS_PROJECT_MANAGER_H
 
-#include "../DataStructures/task.h"
-#include "../DataStructures/List.h"
-#include "../DataStructures/queue.h"
-#include "../DataStructures/priorityQueue.h"
+#include "task.h"
+#include "List.h"
+#include "queue.h"
+#include "priorityQueue.h"
 
 //the manager class handles the logic of the program using the data structures and the tasks.
 
@@ -12,24 +12,19 @@ class manager {
 private:
     queue<task*> q_tasks;
     List<task> l_tasks;
-
+    priorityQueue pq_tasks;
 
     //current user
 public:
     int completedTasks = 0;
     task* currentTask; //for printing the current patient with the doctor
-    //moving pq_tasks to be public
-    priorityQueue pq_tasks;
-
-    //added for tasks display
-    List<task>& getTasks();
 
     task* createTask(Time arrival_time, Time execution_duration, string name, int priority);
     void addtask(task * newTask, int priority);
     void executeTask(task *);
     void updateTasks(Time *globalTime);
     void printAllTasks();
-    void printCompletedTask();
+    void pringCompletedTask();
     //void printNextTask();
     void printCurrentTask();
 
